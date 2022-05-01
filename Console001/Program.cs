b1d030ab2e4258a12d555493786d0778a8fe266b
Serega2976 <serega2976@yandex.ru>
2022-05-01 18:18:53 +0500

    Enter the user name and then greet him

--- Console001/Program.cs
+++ Console001/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, C#!");
+Console.WriteLine("Назовите свое имя?");// Requesting a user name
+string? name = Console.ReadLine(); //Enter the user name
+Console.WriteLine($"Hello, {name}");// Welcome the user
+
